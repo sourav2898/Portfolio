a5bd6f456ad2baf29bef0b8dbe300ce73a5f7407
Sourav Kumar <souravkumardubey2898@gmail.com>
2020-10-30 15:39:51 +0530

    first commit without button styling

---
+++ javascript.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    
+    $('.scroll-up-btn').click(function(){
+       $('html').animate({scrollTop: 0}); 
+    });
+    
+    var typed = new Typed(".typing",{
+        strings: ["Computer Science Engineer.","Developer.","Designer.","Freelancer."], 
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+    var typed = new Typed(".typing-2",{
+        strings: ["Computer Science Engineer.","Developer.","Web Designer."], 
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass('active'); 
+        $('.menu-btn i').toggleClass('active');      
+    });
+});
